493c896d4cc2e37a6bc338b3d4e0476ad0fd4e7b
Roderic Campbell <roderic@gmail.com>
2022-07-16 07:00:44 -0700

    Parameterize the url creation with an enum

--- GPXViewer/GPXViewer/Models/ServiceDataSource.swift
+++ GPXViewer/GPXViewer/Models/ServiceDataSource.swift
@@ -12,7 +12,36 @@ enum TrackState {
     case loading
     case error(Error)
 }
+
+struct ServiceConfig {
+    let host: String// = "localhost"
+    let port: Int// = 8080
+    let scheme: String// = "http"
+}
+enum Service {
+    case local
+    case ngrok
+
+    var url: URL? {
+        var c = URLComponents()
+        c.host = config.host
+        c.scheme = config.scheme
+        c.port = config.port
+        return c.url
+    }
+    var config: ServiceConfig {
+        switch self {
+        case .local:
+            return ServiceConfig(host: "localhost", port: 8080, scheme: "http")
+        case .ngrok:
+            return ServiceConfig(host: "38e2dda5cbac.ngrok.io", port: 80, scheme: "https")
+        }
+    }
+}
+
 class ServiceDataSource: ObservableObject {
+    let service: Service = .local
+
     struct TracksPayload: Decodable {
         let items: [Track]
     }
@@ -25,9 +54,7 @@ class ServiceDataSource: ObservableObject {
     }
 
     func fetchGeojson(for track: Track, completion: @escaping (Result<Data, Error>) -> ()) throws {
-        let host = "https://38e2dda5cbac.ngrok.io/tracks/\(track.id)/geojson"
-//        let host = "http://localhost:8080/tracks/\(track.id)/geojson"
-        guard let url = URL(string: host) else {
+        guard let url = service.url?.appendingPathComponent("tracks/\(track.id)/geojson") else {
             throw LoadingError.invalidURL
         }
 
@@ -50,12 +77,12 @@ class ServiceDataSource: ObservableObject {
         }
     }
     func fetch() {
-        let host = "https://38e2dda5cbac.ngrok.io/tracks"
-//        let host = "http://localhost:8080/tracks"
-        guard let url = URL(string: host) else {
+        guard let url = service.url else {
             return
         }
-        fetcher.fetch(from: url, type: TracksPayload.self) { tracks in
+
+        print(url.appendingPathComponent("tracks"))
+        fetcher.fetch(from: url.appendingPathComponent("tracks"), type: TracksPayload.self) { tracks in
             DispatchQueue.main.async {
                 switch tracks {
 
