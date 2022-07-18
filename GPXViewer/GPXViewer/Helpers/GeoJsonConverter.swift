e6ffbd6fc7eac16b9e348eea2a7bcea2af530ebb
Roderic Campbell <roderic@gmail.com>
2022-07-17 21:28:39 -0700

    Create new GeoJsonError

--- GPXViewer/GPXViewer/Helpers/GeoJsonConverter.swift
+++ GPXViewer/GPXViewer/Helpers/GeoJsonConverter.swift
@@ -1,16 +1,21 @@
 import MapKit
 
+enum GeoJsonError: Error {
+    case noGeometryDetected
+    case notAPolyline
+}
+
 struct GeoJsonConverter {
   public func parseGeoJson(from data: Data) throws -> MKPolyline {
     let mapkitDecoder = MKGeoJSONDecoder()
     do {
       let geoJson = try mapkitDecoder.decode(data) as? [MKGeoJSONFeature]
       guard let geometry = geoJson?.first?.geometry.first else {
-        throw LoadingError.noGeometryDetected
+        throw GeoJsonError.noGeometryDetected
       }
 
       guard let polyline = geometry as? MKPolyline else {
-        throw LoadingError.notAPolyline
+        throw GeoJsonError.notAPolyline
       }
       return polyline
     }
